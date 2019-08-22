d2831c62848517148cde4ae20fd3addbf80870f0
Vittorio <zampinetti@gmail.com>
2019-08-22 14:23:07 +0200

    Various fixes for new dataset to handle float coordinates, *.tiff files, invalid markers extra name suffixes

--- markers.py
+++ markers.py
@@ -5,7 +5,7 @@ from ij import IJ
 
 
 def invert_y(y, height=150):
-    return str(height - int(y))
+    return str(height - int(float(y)))
 
 
 def read_marker(marker_path, y_inv_height=None, to_int=False):
@@ -22,29 +22,32 @@ Read a marker file (any extension) and return the feature of interests (x, y, z)
     :return: Rows containing the coordinates (list of lists)
     """
     rows = []
+    IJ.log('Reading marker {}...'.format(marker_path))
     with open(marker_path, 'r') as marker:
         reader = csv.reader(marker)
         # skip header
         reader.next()
         for row in reader:
+            try:
+                # y coordinate inversion
+                if y_inv_height is not None:
+                    row[1] = invert_y(row[1], y_inv_height)
 
-            # y coordinate inversion
-            if y_inv_height is not None:
-                row[1] = invert_y(row[1], y_inv_height)
-
-            # takes only the x,y,z coordinates (int type if specified)
-            if to_int:
-                row = map(int, row[:3])
-            else:
-                row = row[:3]
-            rows.append(row)
+                # takes only the x,y,z coordinates (int type if specified)
+                if to_int:
+                    row = map(lambda x: int(float(x)), row[:3])
+                else:
+                    row = row[:3]
+                rows.append(row)
+            except IndexError as e:
+                IJ.log('ERROR '+str(e)+' in file '+marker_path)
 
         IJ.log('Read {} rows from {}'.format(len(rows), marker_path))
     return rows
 
 
-def markers_to_csv(source_dir, target_dir=None, y_inv_height=None):
-    # type: (str, str, int) -> None
+def markers_to_csv(source_dir, target_dir=None, y_inv_height=None, extra_suff=''):
+    # type: (str, str, int, str) -> None
     """
 Convert *.marker files in *.csv extracting only the features of interest (i.e. x,y,z)
     :param source_dir: Source directory for the marker files
@@ -52,6 +55,8 @@ Convert *.marker files in *.csv extracting only the features of interest (i.e. x
     :param target_dir: Target directory for the csv files (same as source if None)
 
     :param y_inv_height: Height of the image to invert upside-down the y coord (only if y inversion needed)
+
+    :param extra_suff: Suffix before .tif.marker extension (e.g. '-GT' for files in first SST-11 dataset)
     """
     if target_dir is None:
         target_dir = source_dir
@@ -63,11 +68,12 @@ Convert *.marker files in *.csv extracting only the features of interest (i.e. x
             marker_path = os.path.join(source_dir, filename)
             header = ['x', 'y', 'z']
             rows = read_marker(marker_path, y_inv_height=y_inv_height)
-
-            csv_path = marker_path.replace('-GT.tif.marker', '.csv').replace(source_dir, target_dir)
+            img_path = marker_path.replace('.marker', '')
+            img_name, img_extension = os.path.splitext(img_path)
+            csv_path = (img_name+'.csv').replace(source_dir, target_dir)
             with open(csv_path, 'w') as csv_file:
 
-                IJ.log('Writing rows on {}...'.format(csv_path))
+                IJ.log('Writing {} rows on {}...'.format(len(rows), csv_path))
 
                 writer = csv.writer(csv_file)
                 writer.writerow(header)

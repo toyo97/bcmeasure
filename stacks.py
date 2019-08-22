7dbf7df8251e109eb6ee98910502c4d1c476cf1b
Vittorio <zampinetti@gmail.com>
2019-08-22 15:17:17 +0200

    New parameter to handle different depth res

--- stacks.py
+++ stacks.py
@@ -9,26 +9,26 @@ def square_roi(xc, yc, dim):
     return Roi(x, y, dim, dim)
 
 
-def calculate_slices(stack, zc, dim):
+def calculate_slices(stack, zc, depth):
     # type: (ImageStack, int, int) -> (int, int)
     """
 Calculate the starting slice and the size of the stack
     :param stack: ij.ImageStack, Stack of the original image
     :param zc: Z index of the cell center
-    :param dim: Dimension of the cube roi around the cell
+    :param depth: Depth of the resulting stack
     :return: Starting slice and size
     """
-    z0 = zc - int(dim/2)
+    z0 = zc - int(depth / 2)
     z0 = z0 if z0 >= 1 else 1
 
-    z1 = zc + int(dim/2)
+    z1 = zc + int(depth / 2)
     z1 = z1 if z1 <= stack.getSize() else stack.getSize()
     return z0, z1 - z0 + 1
 
 
 class CellStack(VirtualStack):
-    def __init__(self, stack, xc, yc, zc, dim):
-        # type: (ImageStack, int, int, int, int) -> CellStack
+    def __init__(self, stack, xc, yc, zc, dim, voxel_depth=1):
+        # type: (ImageStack, int, int, int, int, float) -> CellStack
         """
 Constructor of the VirtualStack pointing to the cell at the given coordinates
         :param stack: ij.ImageStack, Stack of the original image
@@ -37,7 +37,7 @@ Constructor of the VirtualStack pointing to the cell at the given coordinates
         :param zc: Z coord of the cell center
         :param dim: Dimension of the cube roi around the cell
         """
-        self.z_start, size = calculate_slices(stack, zc, dim)
+        self.z_start, size = calculate_slices(stack, zc, dim*voxel_depth)
         self.cell_roi = square_roi(xc, yc, dim)
         width = int(self.cell_roi.getFloatWidth())
         height = int(self.cell_roi.getFloatHeight())
